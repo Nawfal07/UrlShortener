e9eb4299d8ea6b5225b4dccada321eb5fd558567
Nawfal Hamdi <nawfal.kenitra07@gmail.com>
2023-01-16 21:56:14 +0100

    add url encoded

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use("/shorten", router);
 
