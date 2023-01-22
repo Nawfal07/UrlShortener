e3a617f1ed1b35dd8ae0e0b1e5c070fc1609b26f
Nawfal Hamdi <nawfal.kenitra07@gmail.com>
2023-01-22 07:54:08 +0100

    server static client

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import cors from "cors";
+import path from "path";
 import connectDB from "./database/db";
 import router from "./routes/urlRoutes";
 dotenv.config();
@@ -11,11 +12,17 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-// app.options("*", cors()); // include before other routes
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
+app.use(express.static(path.join(__dirname, "..", "..", "client", "build")));
 app.use("/shorten", router);
+
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "..", "client", "build", "index.html")
+  );
+});
 
 app.get("/health", (_, res) => {
   return res.send("Server is up");
